ff4cef061174bf0941af9cc4b95e958420d590d6
3060382701 <3060382701@qq.com>
2019-01-18 11:50:02 +0800

    超:增加发送信息的util

---
+++ computer/src/main/java/com/peanut/Capplication.java
@@ -0,0 +1,12 @@
+package com.peanut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Capplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Capplication.class,args);
+    }
+}
